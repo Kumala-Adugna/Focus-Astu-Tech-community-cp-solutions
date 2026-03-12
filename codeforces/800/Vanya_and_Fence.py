90143bf4bb0b82496fc7b5cda6fa7b1fbacda0ce
Kumala <Kumalaadugna04@gmail.com>
2026-03-12 12:29:25 +0300

    Add Codeforces 800: Vanya and Fence solution

---
+++ codeforces/800/Vanya_and_Fence.py
@@ -0,0 +1,13 @@
+# Read input
+n, h = map(int, input().split())
+a = list(map(int, input().split()))
+
+# Calculate minimum width
+width = 0
+for height in a:
+    if height <= h:
+        width += 1
+    else:
+        width += 2
+
+print(width)
